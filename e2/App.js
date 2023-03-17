bfabd50aee4510669f54311258d8ed036aa4bfac
Raunaq Singh <>
2023-03-17 11:53:30 -0600

    finished the app with basic navigation

--- e2/App.js
+++ e2/App.js
@@ -6,24 +6,6 @@ import {View, Text, Button, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-// function HomeScreen({navigation}) {
-//   return (
-//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//       <Text>Home Screen</Text>
-//       <Button
-//         title="Go to Details"
-//         onPress={() => {
-//           /* 1. Navigate to the Details route with params */
-//           navigation.navigate('Details', {
-//             itemId: 86,
-//             otherParam: 'anything you want here',
-//           });
-//         }}
-//       />
-//     </View>
-//   );
-// }
-
 function DetailsScreen({route, navigation}) {
   /* 2. Get the param */
   const {itemId, otherParam} = route.params;
