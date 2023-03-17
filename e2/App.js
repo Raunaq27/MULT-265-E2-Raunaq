947c4ef716f675f24b5b8517feef6e48cdcf2c01
Raunaq Singh <>
2023-03-17 11:30:18 -0600

    header with interative component

--- e2/App.js
+++ e2/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 // In App.js in a new project
 
 import * as React from 'react';
@@ -5,23 +6,23 @@ import {View, Text, Button, Image} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function HomeScreen({navigation}) {
-  return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => {
-          /* 1. Navigate to the Details route with params */
-          navigation.navigate('Details', {
-            itemId: 86,
-            otherParam: 'anything you want here',
-          });
-        }}
-      />
-    </View>
-  );
-}
+// function HomeScreen({navigation}) {
+//   return (
+//     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+//       <Text>Home Screen</Text>
+//       <Button
+//         title="Go to Details"
+//         onPress={() => {
+//           /* 1. Navigate to the Details route with params */
+//           navigation.navigate('Details', {
+//             itemId: 86,
+//             otherParam: 'anything you want here',
+//           });
+//         }}
+//       />
+//     </View>
+//   );
+// }
 
 function DetailsScreen({route, navigation}) {
   /* 2. Get the param */
@@ -53,25 +54,56 @@ function LogoTitle() {
   );
 }
 
+// function StackScreen() {
+//   return (
+//     <Stack.Navigator>
+//       <Stack.Screen
+//         name="Home"
+//         component={HomeScreen}
+//         options={{
+//           headerTitle: props => <LogoTitle {...props} />,
+//           headerRight: () => (
+//             <Button
+//               onPress={() => alert('This is a button!')}
+//               title="Info"
+//               color="#000"
+//             />
+//           ),
+//         }}
+//       />
+//     </Stack.Navigator>
+//   );
+// }
 function StackScreen() {
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Home"
         component={HomeScreen}
-        options={{
+        options={({navigation, route}) => ({
           headerTitle: props => <LogoTitle {...props} />,
-          headerRight: () => (
-            <Button
-              onPress={() => alert('This is a button!')}
-              title="Info"
-              color="#000"
-            />
-          ),
-        }}
+          // Add a placeholder button without the `onPress` to avoid flicker
+          headerRight: () => <Button title="Update count" />,
+        })}
       />
     </Stack.Navigator>
   );
+}
+
+function HomeScreen({navigation}) {
+  const [count, setCount] = React.useState(0);
+
+  React.useEffect(() => {
+    // Use `setOptions` to update the button that we previously specified
+    // Now the button includes an `onPress` handler to update the count
+    navigation.setOptions({
+      headerRight: () => (
+        <Button onPress={() => setCount(c => c + 1)} title="Update count" />
+      ),
+    });
+  }, [navigation]);
+
+  return <Text>Count: {count}</Text>;
 }
 const Stack = createNativeStackNavigator();
 
